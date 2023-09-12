a2b6a848a0b9bde5690276fb91faababbcc34183
Pallavi-09 <pallavisingh992@gmail.com>
2023-09-12 23:23:38 +0530

    folder layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageContentComponent } from './page-content/page-content.component';
+import { ContactComponent } from './page-content/contact/contact.component';
+import { HomeComponent } from './page-content/home/home.component';
+import { ProjectSectionComponent } from './page-content/project-section/project-section.component';
+import { ProjectDetailComponent } from './page-content/project-detail/project-detail.component';
+import { SocialComponent } from './social/social.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    PageContentComponent,
+    ContactComponent,
+    HomeComponent,
+    ProjectSectionComponent,
+    ProjectDetailComponent,
+    SocialComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
